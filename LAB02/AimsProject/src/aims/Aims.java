5a2ea0c9ed938485daeac8979858a791f95af8e4
huyensiukawaii <huyendnk1808@gmail.com>
2024-10-28 22:53:37 +0700

    cap nhat lab2

---
+++ LAB02/AimsProject/src/aims/Aims.java
@@ -0,0 +1,22 @@
+package aims;
+
+public class Aims {
+    public static void main(String[] args) {
+
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation", "Roger Allers",87,19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star wars","Science Fiction", "Geogre Lucas",87,24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin","Animation",18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Hung","Horror",20.00f);
+
+        System.out.println("Total cost is: " + anOrder.totalCost());
+        anOrder.removeDigitalVideoDisc(dvd1);   //Remove successfully dvd1
+        System.out.println("Total cost is: " + anOrder.totalCost());
+
+        anOrder.removeDigitalVideoDisc(dvd4);   //Not found DVD
+    }
+}
---
+++ LAB02/AimsProject/src/aims/Cart.java
@@ -0,0 +1,48 @@
+package aims;
+
+public class Cart {
+    public static final int MAX_NUMBER_ORDERED=20;
+    private DigitalVideoDisc itemsOrdered[]= new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+    private int qtyOrdered;
+    public void addDigitalVideoDisc(DigitalVideoDisc disc)
+    {
+        if (qtyOrdered<MAX_NUMBER_ORDERED)
+        {
+            qtyOrdered++;
+            itemsOrdered[qtyOrdered-1]=disc;
+            System.out.println("The disc has been added");
+        }
+        else
+        {
+            System.out.println("The cart is almost full");
+        }
+    }
+    public int removeDigitalVideoDisc(DigitalVideoDisc disc)
+    {
+        if(itemsOrdered[0] ==  null) {
+            System.out.println("Your cart is empty!");
+            return 0;
+        }
+        for(int i =0; i < qtyOrdered;i++) {
+            if(itemsOrdered[i]==disc) {
+                for(int j = i;i<qtyOrdered-1;i++) {
+                    itemsOrdered[i] = itemsOrdered[i+1];
+                }
+                itemsOrdered[qtyOrdered-1] = null;
+                qtyOrdered--;
+                System.out.println("The disc is removed successfully!");
+                return 0;
+            }
+        }
+        System.out.println("Not found DVD");
+        return 0;
+    }
+
+    public float totalCost() {
+        float sum = 0.00f;
+        for (int i=0;i<qtyOrdered;i++) {
+            sum += itemsOrdered[i].getCost();
+        }
+        return sum;
+    }
+}
